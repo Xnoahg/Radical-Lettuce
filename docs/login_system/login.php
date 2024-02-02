1119e19f323a18f3097f71533d67afb316344ca5
Xnoahg <xnoahg@gmail.com>
2024-02-02 11:11:47 -0500

    stuffs

--- docs/login_system/login.php
+++ docs/login_system/login.php
@@ -1,3 +1,39 @@
+<h1>
+    <div class="navigator" style="text-align: center;">
+        <span class="center" style="text-align: center;">
+            <div class="center logo brightness"><img src="../src/logo.png" class="logo center" onclick="window.location.href='https://radicallettuce.com/' "></div>
+            <p class="center">Radical Lettuce</p>
+            <button class="button disabled" disabled="true" onclick="window.location.href='https://radicallettuce.com/login_system/register.php';">
+                <P>Lettuce Account (coming soon)</P>
+            </button>
+            <button class="button game1"  onclick="window.location.href='https://radicallettuce.com/untitled-lettuce/';">
+                <P>[BETA] Untitled Lettuce Remastered</P>
+            </button>
+            <button class="button disabled" disabled="true" onclick="window.location.href='https://radicallettuce.com/media-player/';">
+                <P>Radical Jukebox</P>
+            </button>
+            <button class="button disabled" disabled="true" onclick="window.location.href='https://radicallettuce.com/game?not-announced/';">
+                <P>Lightbulb Joyride</P>
+            </button>
+        </span>
+    </div>
+    
+</h1>
+<div class="endofpage center">
+    <p style=>Radical Lettuce LLC<br>Our Team:</p>
+    <div id="footer-text" animation-iteration-count: infinite; style="text-align: center;"><p id="pulsing center"></p></div>
+    <button class="button button2" onclick="window.location.href='https://www.youtube.com/@radicallettuce3114';">
+        <img src="../src/yt.png" style="width: 60px; height: 40px;">
+    </button>
+    <button class="button button3"  onclick="window.location.href='https://github.com/xnoahg/Radical-Lettuce'">
+        <img src="../src/github.png" style="width: 40px; height: 40px;"></img>
+    </button>
+    <a class="center" href='#/' id='trig1'><button class="button button1" onclick="window.location.href='https://discord.gg/Z6u75A6tA6';">
+        <img src="../src/discord.png" style="width: 40px; height: 40px;">
+    </button></a>
+    <iframe class="center" id='ifrm1' name='ifrm1' src="https://discord.com/widget?id=1083276910169182268&theme=dark" width="280" height="320" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+</div>
+
 <form action="login.php" method="post">
 
 
